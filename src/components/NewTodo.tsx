b43dcd1c8aee59f79a93627b5e236c3ffa9d98ad
Aayush <falcon.bridge2000@gmail.com>
2022-02-12 05:01:05 +0530

    used refs

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,12 +1,24 @@
+import { useRef } from "react";
+
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+
   const submitHandler = (event: React.FormEvent) => {
     event.preventDefault();
+
+    // const enteredText = todoTextInputRef.current?.value; //if we are not sure whether current will be undefined or defined
+    const enteredText = todoTextInputRef.current!.value; // as we are sure that current in the scenario that we use it can't be null
+
+    if (enteredText.trim().length === 0) {
+      //throw an error
+      return;
+    }
   };
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor="text">Enter a Todo</label>
-      <input id="text" type="text" />
+      <input id="text" type="text" ref={todoTextInputRef} />
       <button type="submit">Add todo</button>
     </form>
   );
